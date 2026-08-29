f77be93f8ca2d8713620c0f5db95a312972b2307
Ayas Ibrahim <aayasabraham@gmail.com>
2026-08-29 23:28:24 +0600

    feat: add forgot and reset password routes to authentication

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -7,10 +7,21 @@ const router = Router();
 router.post("/register", AuthController.registerPatient);
 router.post("/login", AuthController.loginUser);
 router.get(
-	"/me",
-	auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
-	AuthController.getMe,
+  "/me",
+  auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
+  AuthController.getMe,
 );
 router.post("/refresh-token", AuthController.refreshToken);
 router.post("/google", AuthController.googleLogin);
+
+router.post(
+  "/forgot-password",
+
+  AuthController.forgotPassword,
+);
+router.post(
+  "/reset-password",
+
+  AuthController.resetPassword,
+);
 export const AuthRoutes = router;
