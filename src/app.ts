f7627ccc8e4d56e80c6fcc9ca4b425191735740c
Ayas Ibrahim <aayasabraham@gmail.com>
2026-08-31 20:53:21 +0600

    feat: add Cloudinary configuration and update SMTP settings in environment file

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,15 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, {
   type Application,
-  NextFunction,
   type Request,
   type Response,
 } from "express";
 import httpStatus from "http-status";
-import z from "zod";
+
 import config from "./app/config";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
-
 
 const app: Application = express();
 
@@ -33,10 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/v1/auth", AuthRoutes);
-
-
-
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
   res.status(httpStatus.OK).json({
@@ -44,6 +38,7 @@ app.get("/", async (req: Request, res: Response) => {
     message: "Welcome to PH Healthcare System Backend",
   });
 });
+app.use("/api/v1/auth", AuthRoutes);
 
 app.use(globalErrorHandler);
 app.use(notFound);
