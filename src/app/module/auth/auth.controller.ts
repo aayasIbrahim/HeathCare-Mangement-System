bcf71a58d1231236753a2a3316119828b5fabc23
Ayas Ibrahim <aayasabraham@gmail.com>
2026-09-03 23:11:43 +0600

    feat: add Bkash payment integration and user profile image upload functionality

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -8,35 +8,13 @@ import { AuthService } from "./auth.service";
 
 
 const registerPatient = catchAsync(async (req: Request, res: Response) => {
-	// const payload = PatientValidation.PatientRegistrationZodSchema.safeParse(req.body);
-
-	// if(!payload.success){
-	// 	console.log(payload.error);
-	// 	console.log(payload.error.issues);
-		
-	// 	throw new Error(payload.error.issues[0].message)
-	// }
-
-	// console.log(payload);
+	  
 
 	const payload = req.body;
 	
 	await AuthService.registerPatient(payload);
 
-	// const { accessToken, refreshToken, user, patient } = result;
-
-	// res.cookie("accessToken", accessToken, {
-	// 	httpOnly: true,
-	// 	secure: false,
-	// 	sameSite: "none",
-	// 	maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
-	// });
-	// res.cookie("refreshToken", refreshToken, {
-	// 	httpOnly: true,
-	// 	secure: false,
-	// 	sameSite: "none",
-	// 	maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-	// });
+	
 
 	sendResponse(res, {
 		statusCode: httpStatus.CREATED,
