ddfe64ca14b16a9e2213bbae3a46237d0ede3851
Ayas Ibrahim <aayasabraham@gmail.com>
2026-09-07 23:35:32 +0600

    feat: add validation schema for doctor application and integrate it in the controller

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -1,3 +1,4 @@
+import { ApplyAsDoctorValidationZodSchema } from "./doctor.validation";
 import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
@@ -9,7 +10,16 @@ const applyAsDoctor = catchAsync(async (req: Request, res: Response) => {
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
   const resume = files?.["resume"] ? files["resume"][0] : null;
   const additionalFiles = files?.["additionalFiles"] || [];
-  const payload = JSON.parse(req.body.data);
+  const zodValidationResult = ApplyAsDoctorValidationZodSchema.safeParse(
+    JSON.parse(req.body.data),
+  );
+
+  if (!zodValidationResult.success) {
+    throw new Error(zodValidationResult.error.issues[0].message);
+  }
+
+  const payload = zodValidationResult.data;
+
   const result = await DoctorService.applyAsDoctor(
     payload,
     resume,
