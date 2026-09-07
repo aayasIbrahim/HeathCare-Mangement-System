ae8c15edf01573c3665687c5b05f8b0ab41a0664
Ayas Ibrahim <aayasabraham@gmail.com>
2026-09-07 23:25:12 +0600

    feat: implement doctor application functionality with file uploads and validation

---
+++ src/app/module/doctor/doctor.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { DoctorService } from "./doctor.service";
+
+const applyAsDoctor = catchAsync(async (req: Request, res: Response) => {
+  ///postman as form data to send respose
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  const resume = files?.["resume"] ? files["resume"][0] : null;
+  const additionalFiles = files?.["additionalFiles"] || [];
+  const payload = JSON.parse(req.body.data);
+  const result = await DoctorService.applyAsDoctor(
+    payload,
+    resume,
+    additionalFiles,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Applied As Doctor Successfuly",
+    data: result,
+  });
+});
+export const DoctorController = {
+  applyAsDoctor,
+};
